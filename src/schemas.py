fe4f1a7f9a237c6ba4ee292a7791eb690976a912
Benjamin Schuster <149620696+benjaminatarticly@users.noreply.github.com>
2025-03-12 13:07:07 +0100

    Fix fetching wrong llm config (#4)
    * fix: get llm config from mgb config (and not pro7 config), for that implement creating an instance from a dynamic zone
    
    * deploy: new version to test,  NOTE: not yet deployed to prod! just the image tag is already set -> deploy after review and merge
    
    ---------
    
    Co-authored-by: benajminatarticly <benajmin@articly>

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,6 @@
+from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 from pydantic import BaseModel
 
@@ -10,3 +11,25 @@ class Article(BaseModel):
     summary: str
     link: str
     text: Optional[str] = None
+    
+    
+@dataclass
+class ChatGPTModel:
+    self_hosted: bool
+    model: str
+    
+COMPONENT_MAP = {
+    "llm-model.chat-gpt": ChatGPTModel
+}
+
+def create_instance_from_dynamic_zone(data: Dict[str, Any]):
+    """Creates a dataclass instance based on __component field, ignoring `id` and `__component`."""
+    component_type = data.get("__component")
+    
+    if not component_type or component_type not in COMPONENT_MAP:
+        raise ValueError(f"Unknown component type: {component_type}")
+    
+    cls = COMPONENT_MAP[component_type]  # Get the class
+    filtered_data = {k: v for k, v in data.items() if k not in {"__component", "id"}}  # Remove __component & id
+    return cls(**filtered_data)
+
